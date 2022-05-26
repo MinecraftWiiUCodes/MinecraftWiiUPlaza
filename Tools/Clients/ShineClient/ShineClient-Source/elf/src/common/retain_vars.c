926bb7210b1904b54362f1b909a1c4db25359207
Korozin <90534409+Korozin@users.noreply.github.com>
2022-05-26 15:30:10 +0000

    Add files via upload

---
+++ Tools/Clients/ShineClient/ShineClient-Source/elf/src/common/retain_vars.c
@@ -0,0 +1,13 @@
+#include <gctypes.h>
+#include "fs_defs.h"
+
+u8 gSettingLaunchPyGecko __attribute__((section(".data"))) = 0;
+u8 gSettingUseUpdatepath __attribute__((section(".data"))) = 0;
+u8 gSettingPadconMode __attribute__((section(".data"))) = 0;
+u8 gCursorInitDone __attribute__((section(".data"))) = 0;
+u8 gPatchSDKDone __attribute__((section(".data"))) = 0;
+u8 gHIDPADEnabled __attribute__((section(".data"))) = 0;
+u8 gEnableDLC __attribute__((section(".data"))) = 0;
+u32 gLoaderPhysicalBufferAddr __attribute__((section(".data"))) = 0;
+u32 gLogUDP __attribute__((section(".data"))) = 0;
+char gServerIP[16] __attribute__((section(".data")));
---
+++ Tools/Clients/ShineClient/ShineClient-Source/elf/src/common/retain_vars.h
@@ -0,0 +1,15 @@
+#ifndef RETAINS_VARS_H_
+#define RETAINS_VARS_H_
+#include <gctypes.h>
+
+extern u8 gSettingLaunchPyGecko;
+extern u8 gSettingUseUpdatepath;
+extern u8 gSettingPadconMode;
+extern u8 gCursorInitDone;
+extern u8 gPatchSDKDone;
+extern u8 gHIDPADEnabled;
+extern u8 gEnableDLC;
+extern u32 gLoaderPhysicalBufferAddr;
+extern char gServerIP[16];
+
+#endif // RETAINS_VARS_H_
